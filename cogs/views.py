061e388789569acb59f266c05c48307c5accee20
Ryan Tanay <ryantanay@newevolution.org>
2016-09-14 22:37:57 -0500

    Created viewsets for the various models

--- cogs/views.py
+++ cogs/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from cogs.models import User, Cog, Note, PersonalNote
+from cogs.serializers import UserSerializer, CogSerializer, NoteSerializer
+from cogs.permissions import CogmakerOrReadOnly, SameTypeOnly, OwnerOnly
+
+
+class CogViewSet(viewsets.ModelViewSet):
+    queryset = Cog.objects.all()
+    serializer_class = CogSerializer
+    permissions_classes = (CogmakerOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class NoteViewSet(viewsets.ModelViewSet):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+    permissions_classes = (SameTypeOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PersonalNoteViewSet(viewsets.ModelViewSet):
+    queryset = PersonalNote.objects.all()
+    serializer_class = NoteSerializer
+    permissions_classes = (OwnerOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
