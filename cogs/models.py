ca995fc380d063d93b96e66e071d920b507d78d6
Ryan Tanay <ryantanay@newevolution.org>
2016-09-14 20:44:11 -0500

    Created models, made most python not executable as I don't really know why/how it got that way in the first place?

--- cogs/models.py
+++ cogs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+USER_TYPES = [('cogmaker', 'Cog Maker'), ('coguser', 'Cog User')]
+
+class User(AbstractUser):
+    usertype = models.CharField(choices=USER_TYPES, max_length=8)
+
+
+class Cog(models.Model):
+    name = models.CharField(max_length=32)
+    tooth_count = models.IntegerField(default=4)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='cogs')
