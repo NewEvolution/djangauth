ad35587911ba09c3f6591ce01a00aa70e2b853bb
Ryan Tanay <ryantanay@newevolution.org>
2016-09-14 22:55:35 -0500

    Adding missing import

--- cogs/serializers.py
+++ cogs/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from cogs.models import User, Cog
+from cogs.models import User, Cog, Note
 
 
 class CogSerializer(serializers.HyperlinkedModelSerializer):
