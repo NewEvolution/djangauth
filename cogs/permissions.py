8d460df11b09d5f416193c65f1ee9da482d4ecef
Ryan Tanay <ryantanay@newevolution.org>
2016-09-14 22:38:20 -0500

    Created permissions for user types and model viewing

---
+++ cogs/permissions.py
@@ -0,0 +1,36 @@
+from rest_framework import permissions
+
+class IsCogmakerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission so only Cog Maker users can make cogs,
+    and only the creator can edit/delete cogs.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        elif request.method == 'POST':
+            return request.user.user_type == 'cogmaker'
+        else:
+            return request.user == obj.owner
+
+
+class SameTypeOnly(permissions.BasePermission):
+    """
+    Custom permission for Cog User only notes.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.user_type == obj.owner.user_type:
+            if request.method in permissions.SAFE_METHODS:
+                return True
+            return request.user == obj.owner
+
+
+class OwnerOnly(permissions.BasePermission):
+    """
+    Custom permission for personal notes.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.owner
