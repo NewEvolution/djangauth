9030dbcb315e55ec5a8c611dfb979ceee4e2f279
Ryan Tanay <ryantanay@newevolution.org>
2016-12-07 11:03:15 -0600

    Service connected, not working yet

--- front/app.js
+++ front/app.js
@@ -1,4 +1,8 @@
 'use strict'
 
 angular.module('Djangauth', ['ngRoute', 'ngCookies'])
-  .constant('apiUrl', 'http://localhost:8000')
+  .constant('apiUrl', 'http://localhost:8000');
+
+angular.module('Djangauth').run(authService => {
+  authService.authorize();
+});
